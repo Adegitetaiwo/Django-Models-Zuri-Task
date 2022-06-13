3afbdd6b465ccc428ad01c2784bae17e85b47c48
Adegite Taiwo <adegitetaiwo24@gmail.com>
2022-06-13 05:51:09 +0100

    improved code

--- I4G012558NWP/blog/models.py
+++ I4G012558NWP/blog/models.py
@@ -1,11 +1,22 @@
+from zlib import DEF_BUF_SIZE
 from django.db import models
 from django.contrib.auth import get_user_model
 
 
 # Create your models here.
 class Post(models.Model):
+
+    ""
     title = models.CharField(max_length=200)
     text = models.TextField()
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now=False, auto_now_add=True)
     published_date = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name_plural = 'Posts'
+
+    def __str__(self) -> str:
+        return (f"{self.title} by {self.author}").__str__()
